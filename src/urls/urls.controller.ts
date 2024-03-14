2a615d5622c0ec377c73b0596e1d93d69f2a33a0
EktaDhawal <51295793+dekta@users.noreply.github.com>
2024-03-15 00:24:50 +0530

    Advanced analytics added

--- src/urls/urls.controller.ts
+++ src/urls/urls.controller.ts
@@ -1,7 +1,11 @@
-import { Body, Controller, Get, Header, Param, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Request, Res } from '@nestjs/common';
 import { UrlsService } from './urls.service';
 import CreateUrlDto from './dto/create-urls.dto';
 import { Response } from 'express';
+import { Public } from 'src/utils/decorators/public';
+import { APIResponse } from 'src/utils/response.util';
+
+
 
 @Controller('urls')
 export class UrlsController {
@@ -9,24 +13,89 @@ export class UrlsController {
     private readonly urlsService: UrlsService,
   ) {}
     
+    // Api for creating short url
     @Post('/shorturl')
-    async shortUrl(@Body()createUrlDto:CreateUrlDto){
-        const url = await this.urlsService.createUrl(createUrlDto)
-        if(url){
-            return url
+    async shortUrl(@Body()createUrlDto:CreateUrlDto,@Request() req){
+        try{
+            const userId = req.user.id;
+            const urlDoc= await this.urlsService.createUrl(userId.toString(),createUrlDto)
+            if(urlDoc){
+                return APIResponse.create(urlDoc,"short url created",201,true)
+            }
+            return APIResponse.badRequest("some error occur while creating url",400,false)
+
         }
-        return {message:"some error occured"}
+        catch(err){
+            return APIResponse.internalServerError(err,false)
+        }
+        
     }
 
-    @Get('/:urlCode')
-    async redirectUrl(@Param('urlCode')urlCode:string ,  @Res() res: Response){
-        res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-        const url = await this.urlsService.getUrl(urlCode)
-        if(url){
-           return res.redirect(301, url);
+    //Retrieve all short URLs details for a user
+    @Get('/allshorturls')
+    async getAllShortUrls(@Request() req){
+        const userId = req.user.id;
+        const query = {userId} 
+        const projection={shortUrl:1,longUrl:1,clicks:1,createdAt:1,urlCode:1}
+        try{
+            const urls = await this.urlsService.findAllUrls(query,projection)
+            if(urls){
+                const totalUrls = urls.length
+                return APIResponse.found({data:urls,totalUrls},"All short urls with details",200,true)
+            }
+            return APIResponse.notfound("No urls found",404,false)
+
         }
-        else{
-            return res.status(404).send('URL not found');
+        catch(err){
+            return APIResponse.internalServerError(err,false)
         }
+        
     }
+
+
+    // short url 
+    @Public()
+    @Get('/:urlCode')
+    async redirectUrl(@Param('urlCode')urlCode:string ,  @Res() res: Response ,@Request() req){
+        try{
+            res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+            const userAgent = req.headers['user-agent']
+            const url = await this.urlsService.redirectUrl(urlCode,userAgent)
+            if(url){
+               return res.redirect(301, url); 
+            }
+            else{
+                return APIResponse.notfound('URL not found',404)
+            }
+        }
+        catch(err){
+            return APIResponse.internalServerError(err,false)
+        }
+        
+    }
+
+
+    // short url analytics
+    @Get('/urlstats/:urlCode')
+    async getUrlStats(@Request() req, @Param('urlCode')urlCode:string){
+        const userId = req.user.id;
+        const query = {userId,urlCode} 
+        const projection={shortUrl:1,longUrl:1,urlStats:1}
+        try{
+            const url = await this.urlsService.findUrlStats(query,projection)
+            if(url){
+                return APIResponse.found(url,"short url stats",200,true)
+
+            }
+            return APIResponse.notfound('URL not found',404)
+
+        }
+        catch(err){
+            return APIResponse.internalServerError(err,false)
+        }
+       
+    }
+
+
+
 }
--- src/urls/urls.module.ts
+++ src/urls/urls.module.ts
@@ -3,9 +3,15 @@ import { UrlsService } from './urls.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Url, UrlSchema } from './urls.entity';
 import { UrlsController } from './urls.controller';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import Env from 'src/utils/env';
 
 @Module({
-  imports:[MongooseModule.forFeature([{ name: Url.name, schema: UrlSchema }]),],
+  imports:[MongooseModule.forFeature([{ name: Url.name, schema: UrlSchema }]), RedisModule.forRoot({
+      type: 'single',
+      url: Env.redis.redis_server,
+    }),
+  ],
   providers: [UrlsService],
   controllers: [UrlsController]
 })
