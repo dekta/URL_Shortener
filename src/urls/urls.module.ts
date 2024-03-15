5f7f256aff68b20811727327a2658b550732fcd4
EktaDhawal <51295793+dekta@users.noreply.github.com>
2024-03-15 15:17:24 +0530

    setup for testing

--- src/urls/urls.module.ts
+++ src/urls/urls.module.ts
@@ -7,12 +7,15 @@ import { RedisModule } from '@nestjs-modules/ioredis';
 import Env from 'src/utils/env';
 
 @Module({
-  imports:[MongooseModule.forFeature([{ name: Url.name, schema: UrlSchema }]), RedisModule.forRoot({
+  imports:[
+    RedisModule.forRoot({
       type: 'single',
       url: Env.redis.redis_server,
     }),
+    MongooseModule.forFeature([{ name: Url.name, schema: UrlSchema }]),  
   ],
   providers: [UrlsService],
-  controllers: [UrlsController]
+  controllers: [UrlsController],
+  exports:[UrlsService]
 })
 export class UrlsModule {}
